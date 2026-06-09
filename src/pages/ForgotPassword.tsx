c22f265fe64a729d8dfdb4347920b79390a5b6c6
AbinRagab <ahmderagabe@gmail.com>
2026-06-09 11:42:31 +0300

    add: forgot password api integeted

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -5,10 +5,13 @@ import InputField from "../components/auth/InputField";
 import { forgotPasswordSchema, type forgotPasswordFormData } from "../schemas/ForgotPasswordSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useState } from "react";
 
 export default function ForgotPassword() {
   
-  
+ const [successSend, setSuccessSend] = useState(false)
+
     const {
         register,
         handleSubmit,
@@ -19,8 +22,35 @@ export default function ForgotPassword() {
 
 
 
-    const onSubmit = (data:forgotPasswordFormData)=>{
-        
+    const onSubmit = async (data:forgotPasswordFormData)=>{
+        try {
+            
+            const response = await fetch(`${import.meta.env.VITE_SUPABASE_URL}/auth/v1/recover`,{
+                method: 'POST',
+                headers: {
+                    'apikey': import.meta.env.VITE_SUPABASE_API_KEY,
+                    'Content-Type':'application/json',
+                },
+                body: JSON.stringify({
+                    email: data.email
+                }) 
+            })
+
+            const result = await response.json();
+            console.log(result);
+            
+            if(result.error_code){
+                throw new Error(result.msg)
+            }
+
+            toast.success("we’ve sent a password reset link.")
+            setSuccessSend(true)
+
+        } catch (error) {
+            console.log(error, 'error');
+            
+            toast(error.message)
+        }
     }
 
     return (<AuthLayout>
@@ -46,7 +76,7 @@ export default function ForgotPassword() {
 
            </form>
 
-           <Link to={'login'} className="text-primary text-body-md flex items-center justify-center gap-1 my-6 ">
+           <Link to={'/login'} className="text-primary text-body-md flex items-center justify-center gap-1 my-6 ">
                 <span><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3.825 9L9.425 14.6L8 16L0 8L8 0L9.425 1.4L3.825 7H16V9H3.825Z" fill="#003D9B"/>
 </svg>
@@ -58,7 +88,7 @@ export default function ForgotPassword() {
 
            <div className="h-0.5 w-full bg-gray-300 my-4 opacity-20"></div>
 
-           <div className="flex items-top gap-3 rounded-lg p-4 mt-8 text-success-dark bg-success-light ">
+          {successSend?<div className="flex items-top gap-3 rounded-lg p-4 mt-8 text-success-dark bg-success-light ">
             <span>
             <svg width="20" height="35" viewBox="0 0 20 35" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M8.6 14.6L15.65 7.55L14.25 6.15L8.6 11.8L5.75 8.95L4.35 10.35L8.6 14.6ZM10 20C8.61667 20 7.31667 19.7375 6.1 19.2125C4.88333 18.6875 3.825 17.975 2.925 17.075C2.025 16.175 1.3125 15.1167 0.7875 13.9C0.2625 12.6833 0 11.3833 0 10C0 8.61667 0.2625 7.31667 0.7875 6.1C1.3125 4.88333 2.025 3.825 2.925 2.925C3.825 2.025 4.88333 1.3125 6.1 0.7875C7.31667 0.2625 8.61667 0 10 0C11.3833 0 12.6833 0.2625 13.9 0.7875C15.1167 1.3125 16.175 2.025 17.075 2.925C17.975 3.825 18.6875 4.88333 19.2125 6.1C19.7375 7.31667 20 8.61667 20 10C20 11.3833 19.7375 12.6833 19.2125 13.9C18.6875 15.1167 17.975 16.175 17.075 17.075C16.175 17.975 15.1167 18.6875 13.9 19.2125C12.6833 19.7375 11.3833 20 10 20Z" fill="#005235"/>
@@ -70,7 +100,7 @@ export default function ForgotPassword() {
            sent a password reset link.
            </span>
 
-           </div>
+           </div> :''} 
 
            <p className="text-label-sm text-slate-mid mt-6 uppercase">Didn't receive the email?</p>
             <button className="w-full py-3 mt-2  rounded-lg  flex items-center justify-center gap-2 bg-surface-low text-button-sm text-button-timer "> <svg width="18" height="21" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
