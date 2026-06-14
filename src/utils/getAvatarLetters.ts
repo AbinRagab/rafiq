c2a66b74407987f0060b2fea2fd5d11dd7e71515
AbinRagab <ahmderagabe@gmail.com>
2026-06-14 12:35:18 +0300

    Add: get Avatar Letters

---
+++ src/utils/getAvatarLetters.ts
@@ -0,0 +1,11 @@
+export const getAvatarLetters = (name?: string) => {
+    if (!name) return 'US';
+  
+    const words = name.trim().split(' ');
+  
+    if (words.length >= 2) {
+      return `${words[0][0]}${words[1][0]}`.toUpperCase();
+    }
+  
+    return name.slice(0, 2).toUpperCase();
+  };
